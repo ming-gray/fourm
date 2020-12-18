db88e4933735a1aed5fa85019b54554bb2f066b5
ming-gray <1063884584@qq.com>
2020-12-18 21:25:16 +0800

    MBG

--- src/main/java/com/example/community/service/UserService.java
+++ src/main/java/com/example/community/service/UserService.java
@@ -2,8 +2,11 @@ package com.example.community.service;
 
 import com.example.community.mapper.UserMapper;
 import com.example.community.model.User;
+import com.example.community.model.UserExample;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -11,17 +14,23 @@ public class UserService {
     private UserMapper userMapper;
 
     public void createOrUpdate(User user){
-        User dbUser = userMapper.findByAccountId(user.getAccountId());
-        if (dbUser == null){
+        UserExample userExample = new UserExample();
+        userExample.createCriteria().andAccountIdEqualTo(user.getAccountId());
+        List<User> users = userMapper.selectByExample(userExample);
+        if (users.size() == 0){
             user.setGmtCreate(System.currentTimeMillis());
             user.setGmtModified(user.getGmtCreate());
             userMapper.insert(user);
         }else {
-            dbUser.setAvatarUrl(user.getAvatarUrl());
-            dbUser.setGmtModified(System.currentTimeMillis());
-            dbUser.setName(user.getName());
-            dbUser.setToken(user.getToken());
-            userMapper.update(dbUser);
+            User dbUser = users.get(0);
+            User updateUser = new User();
+            updateUser.setAvatarUrl(user.getAvatarUrl());
+            updateUser.setGmtModified(System.currentTimeMillis());
+            updateUser.setName(user.getName());
+            updateUser.setToken(user.getToken());
+            UserExample example = new UserExample();
+            example.createCriteria().andIdEqualTo(dbUser.getId());
+            userMapper.updateByExampleSelective(updateUser, example);
         }
 
     }
