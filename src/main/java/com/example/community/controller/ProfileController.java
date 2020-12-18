03e99ecf09becafdd4677aa7fb2a6e9a49ea8a44
ming-gray <1063884584@qq.com>
2020-12-18 11:35:14 +0800

    add interceptor

--- src/main/java/com/example/community/controller/ProfileController.java
+++ src/main/java/com/example/community/controller/ProfileController.java
@@ -1,7 +1,6 @@
 package com.example.community.controller;
 
 import com.example.community.dto.PaginationDTO;
-import com.example.community.mapper.UserMapper;
 import com.example.community.model.User;
 import com.example.community.service.QuestionService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,13 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ProfileController {
-    @Autowired
-    private UserMapper userMapper;
     @Autowired
     private QuestionService questionService;
 
@@ -27,21 +23,8 @@ public class ProfileController {
                           Model model,
                           @RequestParam(name = "page", defaultValue = "1") Integer page,
                           @RequestParam(name = "size", defaultValue = "5") Integer size){
-        User user = null;
-        Cookie[] cookies = request.getCookies();
-        if (cookies != null && cookies.length != 0) {
-            for (Cookie cookie : cookies) {
-                if (cookie.getName().equals("token")) {
-                    String token = cookie.getValue();
-                    user = userMapper.findByToken(token);
-                    if (user != null) {
-                        request.getSession().setAttribute("user", user);
-                    }
-                    break;
-                }
-            }
-        }
 
+        User user = (User)request.getSession().getAttribute("user");
         if (user == null) {
             return "redirect:/";
         }
