f482da29573eaf289c3486ea036d8cb758ce9ad4
ming-gray <1063884584@qq.com>
2020-12-19 09:39:51 +0800

    add exception

---
+++ src/main/java/com/example/community/exception/CustomizeErrorCode.java
@@ -0,0 +1,16 @@
+package com.example.community.exception;
+
+public enum CustomizeErrorCode implements ICustomizeErrorCode{
+    QUESTION_NOT_FOUND("你找到问题不在了，要不要换个试试？");
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+
+    private String message;
+
+    CustomizeErrorCode(String message) {
+        this.message = message;
+    }
+}
