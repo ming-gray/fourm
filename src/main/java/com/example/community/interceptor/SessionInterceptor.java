f3e54a9961909f4eddf761a0da0e8d834cd25c2e
ming-gray <1063884584@qq.com>
2020-12-22 10:52:29 +0800

    add markdown

--- src/main/java/com/example/community/interceptor/SessionInterceptor.java
+++ src/main/java/com/example/community/interceptor/SessionInterceptor.java
@@ -3,6 +3,7 @@ package com.example.community.interceptor;
 import com.example.community.mapper.UserMapper;
 import com.example.community.model.User;
 import com.example.community.model.UserExample;
+import com.example.community.service.NotificationService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.servlet.HandlerInterceptor;
@@ -17,6 +18,9 @@ import java.util.List;
 public class SessionInterceptor implements HandlerInterceptor {
     @Autowired
     private UserMapper userMapper;
+    @Autowired
+    private NotificationService notificationService;
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         Cookie[] cookies = request.getCookies();
@@ -32,6 +36,8 @@ public class SessionInterceptor implements HandlerInterceptor {
                     if (users.size() != 0) {
                         request.getSession().setAttribute("user", users.get(0));
                     }
+                    Long unreadCount = notificationService.unreadCount(users.get(0).getId());
+                    request.getSession().setAttribute("unreadCount", unreadCount);
                     break;
                 }
 
